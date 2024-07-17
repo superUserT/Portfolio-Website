0c5041c29aff40ba56e927fd7f6a24d68da4bea8
superUserT <rantshothabisomail@gmail.com>
2024-07-17 10:14:29 +0200

    added hmaburger menu

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.getElementById("hamburger");
+  const navList = document.getElementById("nav-list");
+
+  hamburger.addEventListener("click", () => {
+    navList.classList.toggle("active");
+  });
+});
